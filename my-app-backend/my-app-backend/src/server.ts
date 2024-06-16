e4cb795f263943dfb42947c7b4729e95db80efbb
Michael Birklett <mbirk2011@gmail.com>
2024-06-16 02:39:36 -0500

    commit 5

--- my-app-backend/my-app-backend/src/server.ts
+++ my-app-backend/my-app-backend/src/server.ts
@@ -11,7 +11,7 @@ app.use(bodyParser.json());       // Enable parsing of JSON bodies
 
 // Set up the PostgreSQL connection pool
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,  // Use the DATABASE_URL environment variable
+  connectionString: 'postgres://ucvfvp7g5tl3jq:pa6add376e509bb38867a0381cce3a8728329516cdcf2f394c223804a2edac741@c2v3jin4rntblb.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d5k37u9lhmatau',  // Use the DATABASE_URL environment variable
   ssl: {
     rejectUnauthorized: false  // Allow self-signed certificates
   }
